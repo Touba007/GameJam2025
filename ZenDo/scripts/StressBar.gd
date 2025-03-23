ad3d3a52092a9f20adead43dd9535545cb119aaf
leblanct <t.leblanc@student.uclouvain.be>
2025-03-23 04:08:01 +0100

    StressBar Fonctionnelle

---
+++ ZenDo/scripts/StressBar.gd
@@ -0,0 +1,21 @@
+extends TextureProgressBar
+
+@export var max_stress: float = 100.0
+var stress: float = 0.0
+@export var stress_speed: float = 5.0  # stress par seconde
+
+func _ready():
+	min_value = 0
+	max_value = max_stress
+	value = stress
+
+func _process(delta):
+	stress += delta * stress_speed
+	stress = clamp(stress, 0, max_stress)
+	value = stress
+
+	# Si stress max atteint, déclenche game over
+	if stress >= max_stress:
+		get_tree().change_scene_to_file("res://scenes/deathscreen.tscn")
+
+
