629244655f2dd0a95fe53927c6ecf674b6a9dd0f
leblanct <t.leblanc@student.uclouvain.be>
2025-03-23 07:35:10 +0100

    Game Over

--- ZenDo/scripts/deathscreen.gd
+++ ZenDo/scripts/deathscreen.gd
@@ -1,11 +1,12 @@
 extends Control
 
-func _ready():
-	$VBoxContainer/CommencerButton.pressed.connect(_on_commencer_pressed)
-	$VBoxContainer/QuitterButton.pressed.connect(_on_quitter_pressed)
 
-func _on_commencer_pressed():
+func _on_rejouer_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_quitter_pressed():
 	get_tree().quit()
+
+
+func _on_menu_principal_pressed():
+	get_tree().change_scene_to_file("res://scenes/home_screen.tscn")
